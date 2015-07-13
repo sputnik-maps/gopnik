be3d9c0190af43de395265151f99d8298a05de1e
Maxim Dementyev <max@orofarne.ru>
2015-07-13 13:24:09 +0300

    fix TileCoord Equals function

--- src/gopnik/coord.go
+++ src/gopnik/coord.go
@@ -9,7 +9,14 @@ type TileCoord struct {
 }
 
 func (tc *TileCoord) Equals(coord *TileCoord) bool {
-	if tc.Zoom == coord.Zoom && tc.X == coord.X && tc.Y == coord.Y && tc.Size == coord.Size {
+	if tc.Zoom == coord.Zoom && tc.X == coord.X && tc.Y == coord.Y && tc.Size == coord.Size && len(tc.Tags) == len(coord.Tags) {
+		for _, tag := range tc.Tags {
+			for _, tag2 := range coord.Tags {
+				if tag2 != tag {
+					return false
+				}
+			}
+		}
 		return true
 	} else {
 		return false
